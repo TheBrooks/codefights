ce2883a0de623142db85b795257ab86c5250f03a
TheBrooks <the.ryan.brooks@gmail.com>
2018-02-05 16:41:49 -0700

    codefight problem solved

---
+++ messageFromBinaryCode.js
@@ -0,0 +1,37 @@
+/*
+You are taking part in an Escape Room challenge designed specifically for programmers. In your efforts to find a clue, you've found a binary code written on the wall behind a vase, and realized that it must be an encrypted message. After some thought, your first guess is that each consecutive 8 bits of the code stand for the character with the corresponding extended ASCII code.
+
+Assuming that your hunch is correct, decode the message.
+
+Example
+
+For code = "010010000110010101101100011011000110111100100001", the output should be
+messageFromBinaryCode(code) = "Hello!".
+
+The first 8 characters of the code are 01001000, which is 72 in the binary numeral system. 72 stands for H in the ASCII-table, so the first letter is H.
+Other letters can be obtained in the same manner.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string code
+
+A string, the encrypted message consisting of characters '0' and '1'.
+
+Guaranteed constraints:
+0 < code.length < 800.
+
+[output] string
+
+The decrypted message.
+*/
+function messageFromBinaryCode(code) {
+    let charArr = [];
+    let codeCopy = code;
+    while(codeCopy != ''){
+        charArr.push(String.fromCharCode(parseInt(codeCopy.slice(0,8),/* radix =*/2)));
+        codeCopy = codeCopy.slice(8);
+    }
+    return charArr.join('');
+}
